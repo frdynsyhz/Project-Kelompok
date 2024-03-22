f3866e4e0e44653f592e2dcb0cbc6cfbe541b841
resturekayasa <163485189+resturekayasa@users.noreply.github.com>
2024-03-22 15:43:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\AuthController;
 
 
 
-<<<<<<< Updated upstream
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('about', [HomeController::class, 'about'])->name('about');
@@ -19,7 +18,3 @@ Route::get('portfolio', [HomeController::class, 'portfolio'])->name('portfolio')
 Route::get('pricing', [HomeController::class, 'pricing'])->name('pricing');
 Route::get('service', [HomeController::class, 'service'])->name('service');
 Route::get('testimoni', [HomeController::class, 'testimoni'])->name('testimoni');
-=======
-Route::get('register',[AuthController::class, 'register'])->name('register');
-Route::post('register',[AuthController::class, 'registerproses'])->name('registerproses');
->>>>>>> Stashed changes
